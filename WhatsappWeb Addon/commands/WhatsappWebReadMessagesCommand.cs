ff5f6eb6c18808cbd8150f009e1fcd40a9ea23c4
Shubham31335 <67220482+Shubham31335@users.noreply.github.com>
2020-07-14 21:25:19 +0530

    Add files via upload

---
+++ WhatsappWeb Addon/commands/WhatsappWebReadMessagesCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.WhatsappWeb
+{
+    [Command(Name = "whatsappweb.readmessages", Tooltip = "This command is used for reading all the messages sent to you on Whatsapp.")]
+    public class WhatsappWebReadmessagesCommand : Command
+    {
+        public WhatsappWebReadmessagesCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+            [Argument(Tooltip = "Name of a variable where the value of a specified attribute will be stored")]
+            public VariableStructure Result { get; set; } = new VariableStructure("result");
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                arguments.Search.Value = "pane-side";
+                arguments.By.Value = "id";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                System.Threading.Thread.Sleep(5000);
+                arguments.Search.Value = "/html/body/div[1]/div/div/div[3]/div/div[2]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                var attributeValue = SeleniumManager.CurrentWrapper.GetTextValue(
+                 arguments,
+                 arguments.Timeout.Value);
+
+                Scripter.Variables.SetVariableValue(arguments.Result.Value, new TextStructure(attributeValue));
+
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
---
+++ WhatsappWeb Addon/commands/whatsappwebviewstatuscommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.WhatsappWeb
+{
+    [Command(Name = "whatsappweb.viewstatus", Tooltip = "This command is used for viewing status from your whatsapp account.")]
+    public class whatsappwebviewstatusCommand : Command
+    {
+        public whatsappwebviewstatusCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+            [Argument(Tooltip = "Name of a variable where the value of a specified attribute will be stored")]
+            public VariableStructure Result { get; set; } = new VariableStructure("result");
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                arguments.Search.Value = "status-v3";
+                arguments.By.Value = "id";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                System.Threading.Thread.Sleep(6000);
+                arguments.Search.Value = "/html/body/div[1]/div/div/div[3]/div/header/div[2]/div/span/div[1]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                var attributeValue = SeleniumManager.CurrentWrapper.GetTextValue(
+                 arguments,
+                 arguments.Timeout.Value);
+
+                Scripter.Variables.SetVariableValue(arguments.Result.Value, new TextStructure(attributeValue));
+
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
