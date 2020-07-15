d05d7b5bb5dba1d156329ed17259825abc8d56f7
khushi1901 <64182019+khushi1901@users.noreply.github.com>
2020-07-15 19:43:37 +0530

    Add files via upload

---
+++ LinkedIn Addon/Commands/LinkedinSendpostCommandcs.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Linkedin
+{
+    [Command(Name = "linkedin.sendpost", Tooltip = "This command is used to send any post on linkedin.")]
+    public class LinkedinSendpostCommand : Command
+    {
+        public LinkedinSendpostCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Enter the name of the person whom you want to send post here")]
+            public TextStructure sendto { get; set; }
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                arguments.Search.Value = "/html[1]/body[1]/div[8]/div[3]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/div[5]/div[1]/div[2]/div[1]/button[1]/span[1]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                System.Threading.Thread.Sleep(2000);
+                arguments.Search.Value = "msg-connections-typeahead__search-field";
+                arguments.By.Value = "class";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+                SeleniumManager.CurrentWrapper.TypeText(arguments.sendto.Value, arguments, arguments.Timeout.Value);
+
+                System.Threading.Thread.Sleep(5000);
+                SeleniumManager.CurrentWrapper.PressKey("enter", arguments, arguments.Timeout.Value);
+                System.Threading.Thread.Sleep(2000);
+                arguments.Search.Value = "msg-form__send-button";
+                arguments.By.Value = "class";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
