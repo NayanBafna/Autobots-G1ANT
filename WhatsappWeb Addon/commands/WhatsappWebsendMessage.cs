8358e0e5d8eff6b5008cceec7cc57d4d3fe63d1f
Shubham31335 <67220482+Shubham31335@users.noreply.github.com>
2020-07-15 15:33:29 +0530

    Add files via upload

---
+++ WhatsappWeb Addon/commands/WhatsappWebsendMessage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.WhatsappWeb
+{
+    [Command(Name = "whatsappweb.sendmessage", Tooltip = "This command is used to send message on whatsappweb.")]
+    public class WhatsappWebSendmessageCommand : Command
+    {
+        public WhatsappWebSendmessageCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Enter the name of the person whom you want to send message here")]
+            public TextStructure name { get; set; }
+            [Argument(Required = true, Tooltip = "Enter the message to be sent here")]
+            public TextStructure message { get; set; }
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+
+                arguments.Search.Value = "/html/body/div[1]/div/div/div[3]/div/header/div[2]/div/span/div[2]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "/html/body/div[1]/div/div/div[2]/div[1]/span/div/span/div/div[1]/div/label/div/div[2]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                SeleniumManager.CurrentWrapper.TypeText(arguments.name.Value, arguments, arguments.Timeout.Value);
+                System.Threading.Thread.Sleep(5000);
+                arguments.Search.Value = "/html/body/div[1]/div/div/div[2]/div[1]/span/div/span/div/div[2]/div[1]/div/div/div[2]/div/div/div[2]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.PressKey("enter", arguments, arguments.Timeout.Value);
+                System.Threading.Thread.Sleep(5000);
+
+                arguments.Search.Value = "/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                System.Threading.Thread.Sleep(1000);
+                SeleniumManager.CurrentWrapper.TypeText(arguments.message.Value, arguments, arguments.Timeout.Value);
+                System.Threading.Thread.Sleep(4000);
+
+                arguments.Search.Value = "/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+
+
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
