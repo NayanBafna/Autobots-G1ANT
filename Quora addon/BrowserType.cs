e22a63056488d19346ffc49d5808095f69f4a589
NayanBafna <67224210+NayanBafna@users.noreply.github.com>
2020-07-16 14:17:30 +0530

    Add files via upload

---
+++ Quora addon/BrowserType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1ANT.Addon.Quora
+
+{
+    public enum BrowserType
+    {
+        Firefox = 0,
+        Chrome = 1,
+        Edge = 2,
+        InternetExplorer = 3
+    }
+}
