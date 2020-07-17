688b39954a4e51aac0c595450abe3bdec0493ce8
NayanBafna <67224210+NayanBafna@users.noreply.github.com>
2020-07-17 21:13:16 +0530

    Add files via upload

---
+++ Quora addon/commands/QuoraGetNotificationsCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Quora
+{
+    [Command(Name ="quora.getnotification",Tooltip ="This command is used to get all notification")]
+    class QuoraGetNotificationsCommand : Command
+    {
+        public QuoraGetNotificationsCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public class Arguments : SeleniumCommandArguments
+        {
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+           
+            [Argument(Tooltip = "Name of a variable where the value of a specified attribute will be stored")]
+            public VariableStructure result { get; set; } = new VariableStructure("result");
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                Thread.Sleep(5000);
+                arguments.Search.Value = "//div[contains(text(),'Notifications')]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                Thread.Sleep(5000);
+                arguments.Search.Value = "//div[@class='q-flex qu-px--large qu-py--medium qu-bg--white qu-alignItems--center qu-justifyContent--center qu-textAlign--center']";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                var attributeValue = SeleniumManager.CurrentWrapper.GetTextValue(
+                 arguments,
+                 arguments.Timeout.Value);
+
+
+                Scripter.Variables.SetVariableValue(arguments.result.Value, new TextStructure(attributeValue));
+
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
