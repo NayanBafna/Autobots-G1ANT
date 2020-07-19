1d16a0753bfcaf231f0717c7e75e3e80bcc68105
ameyajoshi0503 <56110323+ameyajoshi0503@users.noreply.github.com>
2020-07-19 13:18:14 +0530

    Add files via upload

---
+++ Zomato Addon/Commands/ZomatoLoginCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+
+
+namespace G1ANT.Addon.ZomatoLogin
+{
+    [Command(Name = "zomato.login", Tooltip = "This command is used to login in to the zomato site.")]
+    class ZomatoLoginCommand : Command
+    {
+
+        public ZomatoLoginCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Enter the Login e-mail ID here")]
+            public TextStructure email { get; set; }
+
+            [Argument(Required = true, Tooltip = "Enter the password here")]
+            public TextStructure pword { get; set; }
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumWrapper wrapper = SeleniumManager.CreateWrapper(
+                        "chrome",
+                        "zomato.com",
+                        arguments.Timeout.Value,
+                        false,
+                        Scripter.Log,
+                        Scripter.Settings.UserDocsAddonFolder.FullName);
+                int wrapperId = wrapper.Id;
+                OnScriptEnd = () =>
+                {
+                    SeleniumManager.DisposeAllOpenedDrivers();
+                    SeleniumManager.RemoveWrapper(wrapperId);
+                    SeleniumManager.CleanUp();
+                };
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
