3dc5a2bf0d1604182d7272e21a198798f2cf9693
Shubham31335 <67220482+Shubham31335@users.noreply.github.com>
2020-07-20 13:31:38 +0530

    Add files via upload

---
+++ Amazon addon/Commands/Amazon.login.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Amazon
+
+{
+    [Command(Name = "amazon.login", Tooltip = "This command is used to login in to the amazon site.")]
+    public class TwitterLoginCommand : Command
+    {
+        public TwitterLoginCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Enter the Login e-mail ID here")]
+            public TextStructure email { get; set; }
+
+            [Argument(Required = true, Tooltip = "Enter the password here")]
+            public TextStructure pword { get; set; }
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumWrapper wrapper = SeleniumManager.CreateWrapper(
+                       "chrome",
+                       "amazon.in",
+                        arguments.Timeout.Value,
+                       false,
+                       Scripter.Log,
+                       Scripter.Settings.UserDocsAddonFolder.FullName);
+                int wrapperId = wrapper.Id;
+                OnScriptEnd = () =>
+                {
+                    SeleniumManager.DisposeAllOpenedDrivers();
+                    SeleniumManager.RemoveWrapper(wrapperId);
+                    SeleniumManager.CleanUp();
+                };
+                arguments.Search.Value = "/html/body/div[1]/header/div/div[1]/div[1]/a";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "/html/body/div[3]/div[2]/a[2]/div/div[2]/div";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div[1]/form/div/div/div/div[1]/input[1]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                SeleniumManager.CurrentWrapper.TypeText(arguments.email.Value, arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div[1]/form/div/div/div/div[2]/span/span/input";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "/html/body/div[1]/div[1]/div[2]/div/div[2]/div[1]/div/div/form/div/div[1]/input";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                SeleniumManager.CurrentWrapper.TypeText(arguments.email.Value, arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "/html/body/div[1]/div[1]/div[2]/div/div[2]/div[1]/div/div/form/div/div[2]/span/span/input";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                var len = SeleniumManager.CurrentWrapper.RunScript("return document.getElementsByClassName(\"captcha -internal\").length");
+                if (len == "1")
+                {
+                    RobotMessageBox.Show("Captcha detected, please solve the captcha");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
