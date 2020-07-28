d2c466c43c7d3e3aac97bee5f9b78c5bc7c50f89
NayanBafna <67224210+NayanBafna@users.noreply.github.com>
2020-07-28 19:02:55 +0530

    Add files via upload

---
+++ Medium addon/commands/MediumMuteCommand.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Medium
+{
+    [Command(Name ="medium.mute",Tooltip ="This command is used to mute either author or publication on medium's site")]
+    class MediumMuteCommand : Command
+    {
+        public MediumMuteCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+
+        public class Arguments : SeleniumCommandArguments
+        {
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+
+            [Argument(Required = true, Tooltip = "Enter what to mute, either Author or Publication")]
+            public TextStructure mute { get; set; } = new TextStructure("Author");
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                Thread.Sleep(5000);
+                arguments.Search.Value = "//div//div//div//div//div//div[2]//article[1]//div[1]//div[1]//button[2]//span[1]//*[local-name()='svg']";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                Thread.Sleep(5000);
+                if (arguments.mute.Value == "Publication")
+                {
+                    arguments.Search.Value = "//button[contains(text(),'Mute this publication')]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                }
+                else
+                {
+                    arguments.Search.Value = "//button[contains(text(),'Mute this author')]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+
+
+
+
+    }
+}
