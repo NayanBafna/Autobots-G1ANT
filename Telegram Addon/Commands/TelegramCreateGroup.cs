f9ffd512bc1da40442e0ec99c3ad17d1a1488d16
Shubham31335 <67220482+Shubham31335@users.noreply.github.com>
2020-07-29 21:19:19 +0530

    Add files via upload

---
+++ Telegram Addon/Commands/TelegramCreateGroup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Telegram
+{
+    [Command(Name = "telegram.creategroup", Tooltip = "This command is used to search on the telegram site.")]
+    public class TelegramCreateGroup : Command
+    {
+        public TelegramCreateGroup(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Name = "search", Required = true, Tooltip = "Enter the value to search here")]
+            public TextStructure searchthis { get; set; }
+            [Argument(Name = "groupname", Required = true, Tooltip = "Enter the value to search here")]
+            public TextStructure groupname { get; set; }
+
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+
+                arguments.Search.Value = "/html/body/div[1]/div[1]/div/div/div[1]/div/a/div/span[2]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "/html/body/div[1]/div[1]/div/div/div[1]/div/ul/li[1]/a";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "/html/body/div[6]/div[2]/div/div/div[2]/div[1]/input";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+                SeleniumManager.CurrentWrapper.TypeText(arguments.searchthis.Value, arguments, arguments.Timeout.Value);
+
+                
+                arguments.Search.Value = "/html/body/div[6]/div[2]/div/div/div[2]/div[2]/div/div[1]/ul/li[1]/a";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "/html/body/div[6]/div[2]/div/div/div[3]/div/button[2]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "/html/body/div[6]/div[2]/div/div/div[1]/form/div/input";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+                SeleniumManager.CurrentWrapper.TypeText(arguments.groupname.Value, arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "/html/body/div[6]/div[2]/div/div/div[2]/button[2]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
