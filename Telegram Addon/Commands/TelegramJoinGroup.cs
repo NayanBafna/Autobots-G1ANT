3bdc086f94725e0fc3fa5cd1ae57bbb2665be9cc
Shubham31335 <67220482+Shubham31335@users.noreply.github.com>
2020-07-29 21:22:16 +0530

    Add files via upload

---
+++ Telegram Addon/Commands/TelegramJoinGroup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using G1ANT.Addon.Telegram;
+using G1ANT.Language;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Internal;
+using System.Threading;
+using Keys = System.Windows.Forms.Keys;
+using Docker.DotNet.Models;
+
+namespace G1ANT.Addon.Telegram
+{
+    [Command(Name = "Telegram.joingroup", Tooltip = "This command is used to join group on telegram site.")]
+    class TelegramJoinGroupCommand : Command
+    {
+        public TelegramJoinGroupCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Enter group link here")]
+            public TextStructure sharelink { get; set; }
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+
+                var baseURL = String.Format("https://web.telegram.org/#/im", arguments.Timeout.Value);
+                var URL = String.Concat(baseURL, arguments.sharelink.Value);
+                SeleniumManager.CurrentWrapper.NewTab(arguments.Timeout.Value, URL, true);
+                Thread.Sleep(10000);
+                
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+
+        }
+
+
+    }
+}
