a5923cf5219a0902aebc4a0218679adb674648d9
NayanBafna <67224210+NayanBafna@users.noreply.github.com>
2020-07-30 18:36:46 +0530

    Add files via upload

---
+++ Medium addon/commands/MediumGetNotificationCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Medium
+{
+    [Command(Name ="medium.getnotification", Tooltip ="This command is used to get all the notification")]
+    class MediumGetNotificationCommand : Command
+    {
+        public MediumGetNotificationCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+            
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+
+            [Argument(Tooltip = "Name of a variable where the value of a specified attribute will be stored")]
+            public VariableStructure result { get; set; } = new VariableStructure("result");
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                Thread.Sleep(5000);
+                arguments.Search.Value = "//body/div/div/div/div/div/div/div/button/span[1]//*[local-name()='svg']";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                Thread.Sleep(4000);
+                arguments.Search.Value = "//html//body//div//div//div//div//div//div//ul//li";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                Thread.Sleep(2000);
+                var attributeValue =SeleniumManager.CurrentWrapper.GetTextValue(arguments, arguments.Timeout.Value);
+
+                Scripter.Variables.SetVariableValue(arguments.result.Value, new TextStructure(attributeValue));
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+
+
+
+
+    }
+}
