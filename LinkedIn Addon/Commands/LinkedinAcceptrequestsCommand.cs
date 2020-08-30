86cf716402a0538cdc8be791707770a44efaa9eb
khushi1901 <64182019+khushi1901@users.noreply.github.com>
2020-08-31 00:53:13 +0530

    Add files via upload

---
+++ LinkedIn Addon/Commands/LinkedinAcceptrequestsCommand.cs
@@ -0,0 +1,114 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Linkedin
+{
+    [Command(Name = "linkedin.acceptrequests", Tooltip = "This command is used to accept connection request")]
+    class LinkedinAcceptRequestsCommand : Command
+    {
+        public LinkedinAcceptRequestsCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+
+            [Argument(Required = true, Tooltip = " Enter the number from 1 to 5 to provide how many connection request to be accepted")]
+            public IntegerStructure number { get; set; } = new IntegerStructure("1");
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                Thread.Sleep(8000);
+                arguments.Search.Value = "mynetwork-tab-icon";
+                arguments.By.Value = "id";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                Thread.Sleep(5000);
+                if (arguments.number.Value == 2)
+                {
+                    arguments.Search.Value = "/html[1]/body[1]/div[8]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/ul[1]/li[1]/div[1]/div[2]/button[2]/span[1]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                    Thread.Sleep(3000);
+                    arguments.Search.Value = "/html[1]/body[1]/div[8]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/ul[1]/li[2]/div[1]/div[2]/button[2]/span[1]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                }
+                else if (arguments.number.Value == 3)
+                {
+                    arguments.Search.Value = "/html[1]/body[1]/div[8]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/ul[1]/li[1]/div[1]/div[2]/button[2]/span[1]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                    Thread.Sleep(3000);
+                    arguments.Search.Value = "/html[1]/body[1]/div[8]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/ul[1]/li[2]/div[1]/div[2]/button[2]/span[1]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                    Thread.Sleep(3000);
+                    arguments.Search.Value = "/html[1]/body[1]/div[8]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/ul[1]/li[3]/div[1]/div[2]/button[2]/span[1]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                }
+                else if (arguments.number.Value == 4)
+                {
+                    arguments.Search.Value = "/html[1]/body[1]/div[8]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/ul[1]/li[1]/div[1]/div[2]/button[2]/span[1]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                    Thread.Sleep(3000);
+                    arguments.Search.Value = "/html[1]/body[1]/div[8]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/ul[1]/li[2]/div[1]/div[2]/button[2]/span[1]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                    Thread.Sleep(3000);
+                    arguments.Search.Value = "/html[1]/body[1]/div[8]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/ul[1]/li[3]/div[1]/div[2]/button[2]/span[1]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                    Thread.Sleep(3000);
+                    arguments.Search.Value = "/html[1]/body[1]/div[8]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/ul[1]/li[4]/div[1]/div[2]/button[2]/span[1]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                }
+                else if (arguments.number.Value == 5)
+                {
+                    arguments.Search.Value = "/html[1]/body[1]/div[8]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/ul[1]/li[1]/div[1]/div[2]/button[2]/span[1]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                    Thread.Sleep(3000);
+                    arguments.Search.Value = "/html[1]/body[1]/div[8]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/ul[1]/li[2]/div[1]/div[2]/button[2]/span[1]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                    Thread.Sleep(3000);
+                    arguments.Search.Value = "/html[1]/body[1]/div[8]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/ul[1]/li[3]/div[1]/div[2]/button[2]/span[1]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                    Thread.Sleep(3000);
+                    arguments.Search.Value = "/html[1]/body[1]/div[8]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/ul[1]/li[4]/div[1]/div[2]/button[2]/span[1]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                    Thread.Sleep(3000);
+                    arguments.Search.Value = "/html[1]/body[1]/div[8]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/ul[1]/li[5]/div[1]/div[2]/button[2]/span[1]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                }
+                else
+                {
+                    arguments.Search.Value = "/html[1]/body[1]/div[8]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/ul[1]/li[1]/div[1]/div[2]/button[2]/span[1]";
+                    arguments.By.Value = "xpath";
+                    SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
+
